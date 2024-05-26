d08a6402012ba64e6c2d60461bd528c4bace6c5d
gladkiy-K <gladkiykir@yandex.com>
2024-05-26 23:41:52 +0300

    lets add some code

--- nextFile.java
+++ nextFile.java
@@ -1,3 +1,33 @@
 class Solution {
-	add changes
+	public int[] maxSlidingWindow(int[] nums, int k){
+		if (nums.length == 0) return new int[0];
+
+		//priority queue to keep track of max values
+		PriorityQueue<Integer> maxHeap = new PriorityQueue(100, new maxHeap());
+		int[] result = new int[nums.length - (k-1)];
+
+		for(int i = 0; i < k; i++){
+			maxHeap.add[nums[i]];
+		}
+
+		int left = 0;
+		int right = k - 1;
+		int p = 0;
+		
+		while (right < nums.length){
+			result[p++] = maxHeap.peek();
+			maxHeap.remove(nums[left++]);
+			right++;
+			if(right < nums.lenght){
+				maxHeap.add(nums[right])
+			}
+		}
+		return result;
+	}
+
+	public class maxHeap implements Comparator<Integer>{
+		public int compare(Integer x, Integer y){
+			return y - x;
+		}
+	}
 }
